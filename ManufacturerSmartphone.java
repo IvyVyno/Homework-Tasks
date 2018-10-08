2a33b435049265ba51647e17733573b3754d0f07
IvyVyno <ilvn1997@gmail.com>
2018-10-08 15:16:29 +0300

    HomeWork 5

---
+++ ManufacturerSmartphone.java
@@ -0,0 +1,17 @@
+package HW5;
+
+public class ManufacturerSmartphone {
+	
+	public boolean isSmartphoneSoftwareProtected;
+	public boolean isSmartphoneTechnologiesModernEnough;
+	public boolean isFastTroubleshootingAvailable;
+	public boolean isSmartphoneShockResistantEnough;
+	public boolean isCompetitionBetweenManufacturersFailurePossible;
+	public boolean isSmartphoneCostAttractive;
+	public boolean isSmartphoneHardwareAdvanced;
+	public boolean isSmartphoneHardwareProtected;
+	public boolean isBatterySafeEnough;
+	public boolean isBatteryStrongEnough;
+	public boolean isSmartphoneAppearanceRelevant;
+	
+}
---
+++ UsersSmartphone.java
@@ -0,0 +1,12 @@
+package HW5;
+
+public class UsersSmartphone {
+	
+	public boolean isCameraAvailable;
+	public boolean isErgonomic;
+	public boolean isConfidential;
+	public boolean isBatteryStrongEnough;
+	public boolean isSmartphoneAppearanceRelevant;
+	public int cost;
+	
+}
